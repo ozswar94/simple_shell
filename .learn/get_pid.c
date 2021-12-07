90f790f7a4bef009df3820c02deadb65c5581a58
ozswar94 <3630@holbertonschool.com>
2021-12-07 20:38:23 +0000

    rm directory

--- .learn/get_pid.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-
-int main(void)
-{
-	printf("PID=%u, PPID=%u\n", getpid(), getppid());
-	return 0;
-}
--- .learn/shell.h
+++
@@ -1,7 +0,0 @@
-#ifndef SHELL_H
-#define SHELL_H
-
-ssize_t _getline(char **lineptr, size_t *n);
-char **strsplit(char *str, int c);
-
-#endif /* SHELL_H*/
