6a16002f002c4259485a6833f72b008d07a0563f
ozswar94 <3630@holbertonschool.com>
2021-12-07 20:22:56 +0000

    fix betty for strcut

--- shell.h
+++ shell.h
@@ -4,10 +4,15 @@
 #include "str_func.h"
 #include "printf.h"
 
+/**
+ * struct built_in_s- struct for check built-in function
+ * @command: name of command
+ * @built_in: function launch
+ */
 typedef struct built_in_s
 {
-    char command[100];
-    void (*built_in)(char **, char *, int, char *);
+	char command[100];
+	void (*built_in)(char **, char *, int, char *);
 } built_in_t;
 
 extern char **environ;
