9ac80984c7365bc5bc55649b7436549ddb00472c
Neo <neochronos.chaos9@gmail.com>
2021-12-08 18:50:12 +0100

    Modified getnbr.c according to betty style

--- getnbr.c
+++ getnbr.c
@@ -1,25 +1,35 @@
 #include "str_func.h"
 
-int	my_getnbr(char *str)
+/**
+ * my_getnbr - print numbers (as char)
+ * @str: number to print
+ *
+ * Return: str as int
+ */
+
+int my_getnbr(char *str)
 {
-  int	number;
-  int	i;
-  int	n;
-  
-  i = 0;
-  number = 0;
-  n = 1;
-  while (str[i] == '-' || str[i] == '+')
-  {
-      if (str[i] == '-')
-	n = -1 * n;
-      i++;
-  }
-  while(str[i] != '\0' && (str[i] >= '0' && str[i] <= '9'))
-  {
-      number = (number * 10) + (str[i] - '0');
-      i += 1;
-  }
-  return (number * n);
+	int number;
+	int i;
+	int n;
+
+	i = 0;
+	number = 0;
+	n = 1;
+
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+		n = -1 * n;
+		i++;
+	}
+
+	while (str[i] != '\0' && (str[i] >= '0' && str[i] <= '9'))
+	{
+		number = (number * 10) + (str[i] - '0');
+		i += 1;
+	}
+
+	return (number * n);
 }
 
